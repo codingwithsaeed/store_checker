a2eeb78e6e090c130ed87903d39c358a901ed01b
Saeed Ahmadi <codingwithsaeed@gmail.com>
2025-03-09 18:07:31 +0330

    upgade to new original package version

--- test/store_checker_test.dart
+++ test/store_checker_test.dart
@@ -9,7 +9,7 @@ void main() {
 
   setUp(() {
     channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return Source.IS_INSTALLED_FROM_PLAY_STORE;
+      return InstallSource.PLAY_STORE;
     });
   });
 
@@ -18,6 +18,6 @@ void main() {
   });
 
   test('getSource', () async {
-    expect(await StoreChecker.getSource, Source.IS_INSTALLED_FROM_PLAY_STORE);
+    expect(await StoreChecker.getSource, InstallSource.PLAY_STORE);
   });
 }
